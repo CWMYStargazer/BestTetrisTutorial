65e3c907c40754130ac8382f71f14cecea2eb045
s <944329865@qq.com>
2024-05-17 15:50:16 +0800

    .

--- src/map/TetrisMap.java
+++ src/map/TetrisMap.java
@@ -5,7 +5,7 @@ public final class TetrisMap {
      * 关于map：
      *      map[][0]为缓冲区，不出现在屏幕上，避免生成即死
      * */
-    public static final int[][] map=new int[10][22];
+    public static int[][] map=new int[10][22];
     public static final int mapWidth=10;
     public static final int mapHeight=20;
     private int level;
