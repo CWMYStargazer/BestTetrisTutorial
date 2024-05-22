a7a3a225bfb345196fcc9f743f94086ffc44ac4d
s <944329865@qq.com>
2024-05-22 11:28:54 +0800

    修复由于xy分不清导致的绘制图形错误

--- src/map/TetrisMap.java
+++ src/map/TetrisMap.java
@@ -8,7 +8,7 @@ public final class TetrisMap {
      * 关于map：
      *      map[][0]为缓冲区，不出现在屏幕上，避免生成即死
      * */
-    public int[][] map=new int[10][22];
+    public int[][] map=new int[22][10];
     public static final int mapWidth=10;
     public static final int mapHeight=20;
     private int level;
@@ -32,10 +32,14 @@ public final class TetrisMap {
      * 获取掉落块的信息，存入地图中
      * */
     public void setDropBlock(Block b){
+        //调用触底检测函数
+        //if(Check.groundCheck(b,map)){...}
             for(int y = b.getBlockPosY(); y <b.getBlockHeight()+b.getBlockPosY(); y++){
                 for(int x = b.getBlockPosX(); x <b.getBlockLength()+b.getBlockPosX(); x++){
                     //当地图上对应位置为空格时进行存储，块形状为空时不影响
-                    map[y][x] = b.getBlockForm()[b.blockSpin][y -b.getBlockPosY()][x -b.getBlockPosX()];
+                    if(map[y][x]==0) {
+                        map[y][x] = b.getBlockForm()[b.blockSpin][y - b.getBlockPosY()][x - b.getBlockPosX()];
+                    }
                 }
             }
     }
